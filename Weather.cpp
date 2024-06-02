d7b4a1e991f51ab9031afa448fdb7352e20c747b
”Nikita <mandravrotka@gmail.com>
2024-06-03 00:25:17 +0300

    Добавлен class Weather

---
+++ Weather.cpp
@@ -0,0 +1,14 @@
+#include "Weather.h"
+
+
+Weather::Weather() {
+
+	city = "Киров";
+	lon = 49.6601;
+	lat = 58.5966;
+	temperature = 5.69;
+	weather = "дождь";
+	windSpeed = 4.27;
+	clouds = 100;
+
+}
